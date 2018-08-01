6ea6eae56d56896930c7f6ad6e03c4dae3aee142
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2018-08-01 09:48:35 -0500

    initial commit.

--- setup.py
+++ setup.py
@@ -1,39 +1,53 @@
-from setuptools import setup, find_packages
-import os
+# coding=utf-8
+from setuptools import find_packages
+from setuptools import setup
 
-version = '1.0'
+version = '0.1'
 
-setup(name='collective.notifications',
-      version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='collective.notifications',
+    version=version,
+    description=(
+        "Provides notifications for users."
+    ),
+    long_description="%s\n" % (
+        open("README.rst").read(),
+    ),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Franco Pellegrini',
-      author_email='frapell@ravvit.net',
-      url='https://github.com/collective/collective.notifications',
-      license='GPL',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.intid',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={
-        'test': ['plone.app.testing'],
-        },
-      entry_points="""
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent"
+    ],
+    keywords='Plone Notifications',
+    author='enfold',
+    author_email='info@enfoldsystems.com',
+    url='http://svn.plone.org/svn/collective/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'z3c.jbot',
+    ],
+    extras_require={
+        'async': [
+            'plone.app.async',
+        ]
+    },
+    entry_points="""
+      # -*- Entry points: -*-
+
       [z3c.autoinclude.plugin]
       target = plone
-      """,
-      )
+
+      [celery_tasks]
+      notify = collective.notifications.tasks
+      """
+)
--- src/collective/notifications/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- extra stuff goes here -*-
-
-from zope.i18nmessageid import MessageFactory
-
-_ = MessageFactory('collective.notifications')
-
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
